e6f68ecf4eca07799f7de2d1dac52945ca1fad6b
yaojiaoyi <yaojiaoyi@163.com>
2017-03-06 23:55:30 +0800

    删掉shiro配置

---
+++ web-service/src/main/java/com/seed/security/MyAuthenticationFailHandler.java
@@ -0,0 +1,19 @@
+package com.seed.security;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2017/3/4.
+ */
+public class MyAuthenticationFailHandler implements AuthenticationFailureHandler {
+    @Override
+    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.sendRedirect("/page/404");
+    }
+}
